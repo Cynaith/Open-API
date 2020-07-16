d5ae1d4b7db24b447d000582c00ef327edd5083f
Cynaith <894324762@qq.com>
2020-07-16 23:01:35 +0800

    Update ApiController.java

--- src/main/java/com/ly/openapi/controller/ApiController.java
+++ src/main/java/com/ly/openapi/controller/ApiController.java
@@ -5,6 +5,8 @@ import com.ly.openapi.service.ApiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @author: Cynaith
  **/
@@ -13,12 +15,17 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1")
 public class ApiController {
 
+    private static AtomicInteger atomicInteger = new AtomicInteger(0);
     @Autowired
     ApiService apiService;
 
     @RequestMapping("/zgjm")
     public ResponseWrapper zgjm(@RequestParam("word") String word){
+
         String msg = apiService.getMessage(word);
+        atomicInteger.getAndAdd(1);
+        System.out.println(atomicInteger.get());
         return ResponseWrapper.success(msg);
     }
+
 }
